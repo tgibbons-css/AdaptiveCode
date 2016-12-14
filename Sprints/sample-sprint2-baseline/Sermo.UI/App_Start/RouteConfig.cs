72836f165c5ecaf31bb62d8747d67c547a52aea6
tgibbons-css <tgibbons@css.edu>
2016-12-14 14:47:27 -0600

    Added a default home page, index.html,  so you don't have  to run from Room/List to get the app to work.

--- Sprints/sample-sprint2-baseline/Sermo.UI/App_Start/RouteConfig.cs
+++ Sprints/sample-sprint2-baseline/Sermo.UI/App_Start/RouteConfig.cs
@@ -12,12 +12,20 @@ namespace Sermo.UI
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("");             // don't route the home page, index.html
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            // add route for default startup to jump to room list
+            routes.MapRoute(
+                name: "Root",
+                url: "",
+                defaults: new { controller = "Room", action = "List", id = UrlParameter.Optional }
+            );
+
         }
     }
 }
